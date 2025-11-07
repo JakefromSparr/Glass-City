52e17fdb19919e815ad29b34f2c7a3927ea9a8a6
JakefromSparr <jakespencer22@gmail.com>
2025-11-07 12:53:16 -0500

    Add new situation card for helping an old lady

--- situationDeck/situationCards.js
+++ situationDeck/situationCards.js
@@ -1,1 +1,19 @@
+{
+  "name": "Help an Old Lady Cross the Street",
+  "type": "Situation",                // or "Twist"
+  "targetPower": [19, 23],
+  "urgency": 1,                       // # of cards discarded if left unresolved
+
+  "successText": "Such a strong young hero!",
+  "successResult": {
+    "heroicPoints": 2,
+    "casualties": 0
+  },
+
+  "bustText": "You got most of her there…",
+  "bustResult": {
+    "heroicPoints": 0,
+    "casualties": 1
+  }
+}
 
